ce59f670739589662e639e56133c6981ad8a5cb1
Mikolaj <mikolaj.sitek1@gmail.com>
2023-02-07 21:27:55 +0100

    f

--- website/__init__.py
+++ website/__init__.py
@@ -7,7 +7,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'kvU6yeUkQUzEl5ofCwjq'
+    app.config['SECRET_KEY'] = 'kvU6yeUkQUzEl5ofCwjqDupaFIUT'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
--- website/views.py
+++ website/views.py
@@ -8,37 +8,6 @@ from website.models import Note
 
 views = Blueprint('views', __name__)
 
-
-@views.route('/note_gen', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        title = request.form.get('notetitle')
-        content = request.form.get('notecontent')
-
-        if len(title) == 0 or len(content) == 0:
-            flash("Title and content are required!", category='error')
-            # return render_template("gen_note.html", isEmpty=True)
-        else:
-            link = shortuuid.uuid()[0:6]
-            print(link)
-            new_note = Note(title=title, content=content, link=link)
-            db.session.add(new_note)
-            db.session.commit()
-
-            return redirect(f"/notes/{link}")
-    else:
-        return render_template("gen_note.html")
-
-
-@views.route('/notes/<string:link>')
-def notes(link):
-    note = Note.query.filter_by(link=link).first()
-    if note:
-        return render_template("notes.html", note=note)
-    else:
-        return render_template("404.html")
-
-
 @views.route('/')
 def index():
     return render_template("index.html")
